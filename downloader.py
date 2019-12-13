e0491d6f5ef437afd461cbb0314358faf8971c15
JavierPlata <javi.333.6@gmail.com>
2019-12-13 12:25:06 +0100

    Implementados mensajes de los downloaders para informar a los ochestrators de los nuevos archivos descargados

--- downloader.py
+++ downloader.py
@@ -11,6 +11,7 @@ try:
 except ImportError:
     print('ERROR: do you have installed youtube-dl library?')
     sys.exit(1)
+import IceStorm
 
 
 class NullLogger:
@@ -55,6 +56,10 @@ def download_mp3(url, destination='./'):
 
 
 class Downloader(TrawlNet.Downloader):
+    
+    def __init__(self, server):
+        self.serverMaster = server
+    
     def addDownloadTask(self, url, current=None):
         print("Descargando tarea:", url)
         fileName, fileId = download_mp3(url)
@@ -62,18 +67,45 @@ class Downloader(TrawlNet.Downloader):
         fileinfo = TrawlNet.FileInfo()
         fileinfo.name = fileName[2:len(fileName)-1]
         fileinfo.hash = fileId
+        orch = TrawlNet.UpdateEventPrx.uncheckedCast(self.serverMaster.publisher)
+        orch.newFile(fileinfo)
         return fileinfo
         
         
 class Server(Ice.Application):
+    
+    publisher = None
+    
+    def get_topic_manager(self):
+        key = 'IceStorm.TopicManager.Proxy'
+        proxy = self.communicator().propertyToProxy(key)
+        if proxy is None:
+            print ("property {} not set".format(key))
+            return None
+        print("Using IceStorm in: '%s'" % key)
+        return IceStorm.TopicManagerPrx.checkedCast(proxy)
+    
     def run(self, argv):
         broker = self.communicator()
-        servant = Downloader()
+        servant = Downloader(self)
         
         adapter = broker.createObjectAdapter("ServerAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("downloader1"))
         
         print(proxy, flush=True)
+        
+        topic_mgr = self.get_topic_manager()
+        if not topic_mgr:
+            print ('Invalid porxy')
+            return 2
+            
+        topic_name = "UpdateEvents"
+        try:
+            topic = topic_mgr.retrieve(topic_name)
+        except IceStorm.NoSuchTopic:
+            topic = topic_mgr.create(topic_name)
+            
+        self.publisher = topic.getPublisher()
         
         adapter.activate()
         self.shutdownOnInterrupt()
